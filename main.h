f27acea83d5435d12d816fe83af5699a6b4ed4a5
Azizaazu <aynaou.aziza98@gmail.com>
2023-04-28 15:57:47 -0700

    update

--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #define BUFF_SIZE 1024
 #define UNUSED(x) (void)(x)
 
@@ -8,12 +7,9 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-#define UNUSED(x) (void)(x)
-
 /* size */
 #define S_LONG 2
 #define S_SHORT 1
-
 /* flags */
 #define F_MINUS 1
 #define F_PLUS 2
@@ -40,14 +36,12 @@ struct format
  * @func_t: The function associated.
  */
 typedef struct format func_t;
-
 int _printf(const char *fmt, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flag, int width, int prec, int size);
 
 /*Functions */
-
-/* Functions to print numbers */
+/* Functions for numbers */
 int print_unsigned_number(va_list types, char buffer[],
         int flag, int width, int prec, int size);
 int print_octal_number(va_list types, char buffer[],
@@ -62,7 +56,7 @@ int print_binary(va_list types, char buffer[],
 	int flag, int width, int prec, int size);
 int print_hupper_number(va_list types, char buffer[],
 	int flag, int width, int prec, int size);
-/* Funtions to print chars and strings */
+/* Funtions for chars and strings */
 int print_str(va_list types, char buffer[],
         int flag, int width, int prec, int size);
 int print_char(va_list types, char buffer[],
@@ -74,24 +68,19 @@ int is_digit(char);
 int if_num_is_printable(char);
 long int convert_size_unsgnd(unsigned long int n, int size);
 int append_hexa_code(char, char[], int);
-
 long int convert_size_number(long int n, int size);
-
 /* Function for non printable char */
 int print_non_printable_number(va_list types, char buffer[],
 	int flag, int width, int prec, int size);
 /*Function to print string in reverse*/
 int print_reverse_number(va_list types, char buffer[],
         int flag, int width, int prec; int size);
-
 /* Function for  memory address */
 int print_pointer(va_list types, char buffer[],
 	int flag, int width, int prec, int size);
-
 /*Function to print a string in rot 13*/
 int print_rot13edstring(va_list types, char buffer[],
 	int flag, int width, int prec, int size);
-
 /* width handler */
 int write_pointer(char buffer[], int ind, int length,
         int width, int flags, char padd, char extra_c, int padd_start);
@@ -103,7 +92,6 @@ int write_number(int is_positive, int index, char buffer[],
 	int flag, int width, int prec, int size);
 int write_num(int ind, char bff[], int flag, int width, int prec,
 	int l, char padd, char extra_c);
-
 /* handle other specifiers */
 int get_width(const char *fmt, int *i, va_list list);
 int get_precision(const char *fmt, int *i, va_list list);
